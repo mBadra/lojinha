653e9bd19766d487e9f71b26796abac72f1c644c
mBadra <pmbsouza@gmail.com>
2020-02-18 21:00:32 -0300

    teste
    teste

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./sass/styles.scss";
 import Main from "./Main";
-
+//teste
 const App = () => (
   <div>
     <Main />
   </div>
 );
+
 export default App;
